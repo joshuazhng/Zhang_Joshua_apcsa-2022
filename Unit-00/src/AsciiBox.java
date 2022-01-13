488f160c557e708d51463533b254387c806ed39e
joshuazhng <joshuaezhang@gmail.com>
2022-01-13 09:06:22 -0800

    Initial push of Unit 00

---
+++ Unit-00/src/AsciiBox.java
@@ -0,0 +1,17 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("name \t  date \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+
+	}
+}
---
+++ Unit-00/src/Variables.java
@@ -0,0 +1,51 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+
+
+		//decimal variables
+
+
+		//other integer types
+
+
+		//other types
+
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
