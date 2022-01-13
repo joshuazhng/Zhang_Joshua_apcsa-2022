d3f1597f936ca79021dc37cd248af9b4a1088ed4
Joshua Zhang <joshuaezhang@gmail.com>
2022-01-12 23:15:57 -0800

    Adding initial version of Input

---
+++ Input.java
@@ -0,0 +1,43 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class Input
+{
+	public static void main (String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+
+		int intOne, intTwo;
+		double doubleOne, doubleTwo;
+		float floatOne, floatTwo;
+		short shortOne, shortTwo;
+
+
+		System.out.print("Enter an integer :: ");
+		intOne = keyboard.nextInt();
+
+
+		System.out.print("Enter an integer :: ");
+		intTwo = keyboard.nextInt();
+
+
+		//add in input for all variables
+
+
+		System.out.println();
+		System.out.println("integer one = " + intOne );
+		System.out.println("integer two = " + intTwo );
+
+		//add in output for all variables
+
+
+	}
+}
