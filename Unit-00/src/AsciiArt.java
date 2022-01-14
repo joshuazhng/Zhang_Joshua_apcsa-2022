9f64c85a835a87e5ea81701b2e06b8e4f9d7e3ca
joshuazhng <joshuaezhang@gmail.com>
2022-01-13 18:28:52 -0800

    Coded AsciiArt.java

--- Unit-00/src/AsciiArt.java
+++ Unit-00/src/AsciiArt.java
@@ -1,17 +1,17 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Joshua Zhang
+//Date - 1/13/22
+//Class - AP CSA Mauro Period 1
+//Lab  - Ascii Art
 
 public class AsciiArt
 {
 	public static void main ( String[] args )
 	{
-		System.out.println("Your Name \n\n" );
-		System.out.println("What type of ANIMAL YOU WILL DRAW" );
+		System.out.println("Joshua Zhang \t 1/13/22\n\n" );
+		System.out.println("What type of ANIMAL YOU WILL DRAW: Penguin with a Hat" );
 		System.out.println("\n\n\n\n" );
 
 		System.out.println("                /\\                " );
@@ -19,7 +19,15 @@ public class AsciiArt
 		System.out.println("              /    \\              " );
 		System.out.println("             [------]             " );
 		//add other output
-
+		System.out.println("             |      |      " );
+		System.out.println("             | '  ' |      " );
+		System.out.println("             |__<>__|      " );
+		System.out.println("             |      |      " );
+		System.out.println("             | U  U |      " );
+		System.out.println("             |______|      " );
+		System.out.println("             |_|  |_|      " );
+		
+		
 		System.out.println(" \n\n\n\nHelpFul Hints" );
 		System.out.println("\\\\ draws one backslash on the screen!\n" );
 		System.out.println("\\\" draws one double quote on the screen!\n" );
