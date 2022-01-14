95ab5dee2911d320d59c194ab63a15f866922232
joshuazhng <joshuaezhang@gmail.com>
2022-01-13 17:21:41 -0800

    Added initial version of Unit 1

---
+++ Unit-01/src/StarsAndStripes.java
@@ -0,0 +1,38 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+   }
+
+   public void printTwentyDashes()
+   {
+   }
+
+   public void printTwoBlankLines()
+   {
+   }
+   
+   public void printASmallBox()
+   {	
+   }
+ 
+   public void printABigBox()
+   { 	
+   }   
+}
