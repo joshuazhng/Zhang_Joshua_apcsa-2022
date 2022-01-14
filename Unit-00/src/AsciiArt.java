b78425fa910d457557b0ea26ba02ceffa20d7d0c
joshuazhng <joshuaezhang@gmail.com>
2022-01-14 09:16:49 -0800

    Updated AsciiArt helpful hints

--- Unit-00/src/AsciiArt.java
+++ Unit-00/src/AsciiArt.java
@@ -29,8 +29,8 @@ public class AsciiArt
 		
 		
 		System.out.println(" \n\n\n\nHelpFul Hints" );
-		System.out.println("\\\\ draws one backslash on the screen!\n" );
-		System.out.println("\\\" draws one double quote on the screen!\n" );
-		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+		System.out.println("\\ draws one backslash on the screen!\n" );
+		System.out.println("\" draws one double quote on the screen!\n" );
+		System.out.println("\' draws one single quote on the screen!\n" ); 
 	}
 }
