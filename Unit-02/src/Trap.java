bed41b371ecbce5746c90ea7b411933adfb443a2
joshuazhng <joshuaezhang@gmail.com>
2022-01-17 14:25:07 -0800

    Copied initial Unit 2 files

---
+++ Unit-02/src/Trap.java
@@ -0,0 +1,13 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		return 0;
+	}
+}
