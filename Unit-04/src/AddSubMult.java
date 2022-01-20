b074bcd08a5fd1beabce29b06c820bbd09903f2d
joshuazhng <joshuaezhang@gmail.com>
2022-01-19 22:12:44 -0800

    Loaded Unit 4 files

---
+++ Unit-04/src/AddSubMult.java
@@ -0,0 +1,14 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*; 
+import java.util.Scanner;
+
+public class AddSubMult
+{
+	public static double check( double a, double b )
+	{
+		return 0;
+	}
+}
