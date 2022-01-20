25b1a31d0e58af414fd02a3a66d85512c5503da7
joshuazhng <joshuaezhang@gmail.com>
2022-01-19 22:05:14 -0800

    Finished coding Distance lab Unit 3

--- Unit-03/src/Distance.java
+++ Unit-03/src/Distance.java
@@ -17,27 +17,42 @@ public class Distance
 
 	public Distance()
 	{
+		xOne = 0;
+		yOne = 0;
+		xTwo = 0;
+		yTwo = 0;
 	}
 
 	public Distance(int x1, int y1, int x2, int y2)
 	{
+		xOne = x1;
+		yOne = y1;
+		xTwo = x2;
+		yTwo = y2;
 	}
 
 	public void setCoordinates(int x1, int y1, int x2, int y2)
 	{
+		xOne = x1;
+		yOne = y1;
+		xTwo = x2;
+		yTwo = y2;
 	}
 
 	public void calcDistance()
 	{
+		double value = Math.pow(xTwo-xOne, 2) + Math.pow(yOne-yTwo, 2);
+		distance = Math.sqrt(value);
 	}
 	
 	public double getDistance()
 	{
-		return 0.0;
+		return distance;
 	}
 	
 	public void print()
 	{
+		out.printf("distance == %.3f", distance);
 	}
 	
 	//complete print or the toString
--- Unit-03/src/DistanceRunner.java
+++ Unit-03/src/DistanceRunner.java
@@ -14,7 +14,48 @@ public class DistanceRunner
 {
 	public static void main( String[] args )
 	{
+		Scanner keyboard = new Scanner(in);
+		
 		//add test cases	
-			
+		out.print("Enter X1 :: ");
+		int x1 = keyboard.nextInt();
+		out.print("Enter Y1 :: ");
+		int y1 = keyboard.nextInt();
+		out.print("Enter X2 :: ");
+		int x2 = keyboard.nextInt();
+		out.print("Enter Y2 :: ");
+		int y2 = keyboard.nextInt();
+		
+		Distance test1 = new Distance(x1, y1, x2, y2);
+		test1.calcDistance();
+		test1.print();
+		out.println("\n\n");
+		
+		out.print("Enter X1 :: ");
+		x1 = keyboard.nextInt();
+		out.print("Enter Y1 :: ");
+		y1 = keyboard.nextInt();
+		out.print("Enter X2 :: ");
+		x2 = keyboard.nextInt();
+		out.print("Enter Y2 :: ");
+		y2 = keyboard.nextInt();
+		
+		Distance test2 = new Distance(x1, y1, x2, y2);
+		test2.calcDistance();
+		test2.print();
+		out.println("\n\n");
+		
+		out.print("Enter X1 :: ");
+		x1 = keyboard.nextInt();
+		out.print("Enter Y1 :: ");
+		y1 = keyboard.nextInt();
+		out.print("Enter X2 :: ");
+		x2 = keyboard.nextInt();
+		out.print("Enter Y2 :: ");
+		y2 = keyboard.nextInt();
+		
+		Distance test3 = new Distance(x1, y1, x2, y2);
+		test3.calcDistance();
+		test3.print();
 	}
 }
