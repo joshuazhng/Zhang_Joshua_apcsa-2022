05ae6a5d2d842e671b9ae852af0d3713c9ea0e39
joshuazhng <joshuaezhang@gmail.com>
2022-01-24 08:57:00 -0800

    Updated Perfect

--- Unit-06/src/Perfect.java
+++ Unit-06/src/Perfect.java
@@ -9,14 +9,42 @@ public class Perfect
    private int number;
 
 	//add constructors
-
+    public Perfect()
+    {
+    	setNum(0);
+    }
+    
+    public Perfect(int num)
+    {
+    	setNum(num);
+    }
+   
 	//add a set method
-
+    public void setNum(int num)
+    {
+    	number = num;
+    }
+    
 	public boolean isPerfect()
 	{
+		int num=1;
+		int sum=0;
+		while(num < number)
+		{
+			if (number % num == 0) {
+				sum = sum + num;
+			}
+			num++;
+		}
+		if (sum == number) {
+			return true;
+		}
 		return false;
 	}
 
 	//add a toString	
-	
+	public String toString()
+	{
+		return "";
+	}
 }
