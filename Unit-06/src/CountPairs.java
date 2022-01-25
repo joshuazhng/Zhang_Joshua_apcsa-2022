696eacc6e0393bcd0d69ebbd461c92bb2b7a656d
joshuazhng <joshuaezhang@gmail.com>
2022-01-25 09:30:04 -0800

    Coded CountPairs.java

--- Unit-06/src/CountPairs.java
+++ Unit-06/src/CountPairs.java
@@ -9,6 +9,11 @@ public class CountPairs
 	public static int pairCounter( String str )
 	{ 
 		int count = 0;
+		for (int i=0; i<str.length() - 1; i++) {
+			if (str.charAt(i) == str.charAt(i+1)) {
+				count++;
+			}
+		}
 		return count;
 	}
 }
