09261a050293f01542a74a8c30e3e798d9c1aaa3
joshuazhng <joshuaezhang@gmail.com>
2022-01-30 23:14:01 -0800

    Finished Coding Unit 7 Triangle Five

--- Unit-07/src/TriangleFive.java
+++ Unit-07/src/TriangleFive.java
@@ -33,7 +33,25 @@ public class TriangleFive
 
 	public String toString()
 	{
-		String output="";
+ 		String output = "";
+		char currLetter = letter;
+		for (int i = 0; i < amount; i++)
+		{
+			currLetter = letter;
+			for (int j = 0; j < amount-i; j++) //j represents the added # to the ASCII of the letter and the # of words in each line
+			{
+				for (int k = amount; k > j; k--) //k is the number of letters in each word
+				{
+					if (currLetter+j > 'Z')
+						currLetter -= 26;
+					output += (char)(currLetter+j);
+				}
+				output += " ";
+			
+			}
+			
+			output += "\n";
+		}
 		return output;
 	}
 }
--- Unit-07/src/TriangleFiveRunner.java
+++ Unit-07/src/TriangleFiveRunner.java
@@ -8,5 +8,35 @@ public class TriangleFiveRunner
 {
    public static void main(String args[])
    {
-	}
+	   TriangleFive test = new TriangleFive();
+	   test.setLetter('C');
+	   test.setAmount(4);
+	   out.print(test);
+	   out.println();
+	   
+	   test.setLetter('A');
+	   test.setAmount(5);
+	   out.print(test);
+	   out.println();
+	   
+	   test.setLetter('B');
+	   test.setAmount(7);
+	   out.print(test);
+	   out.println();
+	   
+	   test.setLetter('B');
+	   test.setAmount(7);
+	   out.print(test);
+	   out.println();
+	   
+	   test.setLetter('X');
+	   test.setAmount(6);
+	   out.print(test);
+	   out.println();
+	   
+	   test.setLetter('Z');
+	   test.setAmount(8);
+	   out.print(test);
+	   out.println();
+   }
 }
