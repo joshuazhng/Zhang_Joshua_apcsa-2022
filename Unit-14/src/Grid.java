224fa9e15b757b3d9a82e2cd0bef64bcf54b8d82
joshuazhng <joshuaezhang@gmail.com>
2022-03-09 10:38:08 -0800

    Updated Grid

--- Unit-14/src/Grid.java
+++ Unit-14/src/Grid.java
@@ -9,24 +9,63 @@ public class Grid
    //load vals into the rows x cols grid randomly
 	public Grid(int rows, int cols, String[] vals)
 	{
+		grid = new String[rows][cols];
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				int v = (int)Math.random()*vals.length;
+				String val = vals[v];
+				grid[i][j] = val;
+			}
+		}
 	}
 
 	//find out which of the vals occurs the most
 	public String findMax(String[] vals)
 	{
-		return "nothing yet";
+		int max = -1;
+		int count = 0;
+		String most = "";
+		for ( String s : vals) {
+			for ( String[] row : grid) {
+				for (String w : row) {
+					if (w.equals(s)) {
+						count++;
+					}
+				}
+			}
+			if (max < count) {
+				max = count;
+				most = s;
+			}
+			count = 0;
+		}
+		return most;
 	}
 
 	//returns a count of how many times val occurs in the matrix
 	private int countVals( String val )
 	{
-		return 0;
+		int count = 0;
+		for ( String[] row : grid) {
+			for (String s : row) {
+				if (s.equals(val)) {
+					count++;
+				}
+			}
+		}
+		return count;
 	}
 
 	//display the grid
 	public String toString()
 	{
-		String output="";
+		String output = "";
+		for (String[] row : grid) {
+			for (String s : row) {
+				output += s + " ";
+			}
+			output += "\n";
+		}
 		return output;
 	}
 }
--- Unit-14/src/GridRunner.java
+++ Unit-14/src/GridRunner.java
@@ -11,5 +11,8 @@ public class GridRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
+		Grid x = new Grid(7, 7, new String[]{"A", "B", "C", "D", "#", "G", "3"} );
+		System.out.println(x);
+		System.out.println(x.findMax(new String[] {"X", "A", "B"}));
 	}
 }
