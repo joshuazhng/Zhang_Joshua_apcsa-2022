12c19fb682d53501e70649c4becec0c163622144
joshuazhng <joshuaezhang@gmail.com>
2022-03-17 09:14:21 -0700

    Coded NumberSorter

--- Unit-13/src/NumberSorter.java
+++ Unit-13/src/NumberSorter.java
@@ -15,12 +15,35 @@ public class NumberSorter
 	private static int getNumDigits(int number)
 	{
 		int count = 0;
+		while (number > 0) {
+			number = number / 10;
+			count++;
+		}
 		return count;
 	}
 
 	public static int[] getSortedDigitArray(int number)
 	{
-		int[] sorted = null;
+		int[] sorted = new int[getNumDigits(number)];
+		int num = number;
+		for (int i = 0; i < sorted.length; i++) {
+			sorted[i] = num % 10;
+			num = num / 10;
+		}
+		
+		for (int i = 0; i < sorted.length; i++) {
+			int min = i;
+			for (int j = i+1; j < sorted.length; j++) {
+				if (sorted[j] < sorted[min]) {
+					min = j;
+				}
+			}
+			if (min != i) {
+				int temp = sorted[min];
+				sorted[min] = sorted[i];
+				sorted[i] = temp;
+			}
+		}
 		return sorted;
 	}
 }
--- Unit-13/src/NumberSorterRunner.java
+++ Unit-13/src/NumberSorterRunner.java
@@ -9,6 +9,7 @@ public class NumberSorterRunner
 {
 	public static void main(String args[])
 	{
+		
 		int[] cases = {567891, 901912468, 864213507, 898777, 234422};
 		for( int test : cases )
 		{
