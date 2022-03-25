2e275532c2635d80d1b63e4f5acb3915616af493
joshuazhng <joshuaezhang@gmail.com>
2022-03-25 09:24:11 -0700

    Finished Unit 13

--- Unit-13/src/WordSortTwo.java
+++ Unit-13/src/WordSortTwo.java
@@ -12,15 +12,20 @@ public class WordSortTwo
 
 	public WordSortTwo(String sentence)
 	{
+		wordRay = sentence.split(" ");
 	}
 
 	public void sort()
 	{
+		Arrays.sort(wordRay);
 	}
 
 	public String toString()
 	{
-		String output="";
+		String output = "";
+		for (String s : wordRay) {
+			output += s + "\n";
+		}
 		return output+"\n\n";
 	}
 }
--- Unit-13/src/WordSortTwoRunner.java
+++ Unit-13/src/WordSortTwoRunner.java
@@ -12,13 +12,16 @@ public class WordSortTwoRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
-		Scanner file = new Scanner(new File("wordsorttwo.dat"));
+		Scanner file = new Scanner(new File("C:\\Users\\zhangj1616\\Desktop\\APCSA_2022\\Zhang_Joshua_apcsa-2022\\Unit-13\\src\\wordsorttwo.dat"));
 		int size = file.nextInt();
 		file.nextLine();
 		for(int i = 0; i<size; i++)
 		{
 			String sentence = file.nextLine();
 			//instantiate a new WordSort
+			WordSortTwo test = new WordSortTwo(sentence);
+			test.sort();
+			System.out.print(test);
 		}
 	}
 }
